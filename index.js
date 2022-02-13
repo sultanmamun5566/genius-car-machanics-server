4ea795170ae33b3a8f4f8b27b56b612da2b299a2
sultanmamun5566 <sultanmamun5566@gmail.com>
2022-02-13 23:06:03 +0600

    added port

--- index.js
+++ index.js
@@ -5,12 +5,10 @@ require('dotenv').config();
 const ObjectId = require('mongodb').ObjectId;
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-
-
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hqjj7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -32,14 +30,14 @@ async function run() {
         app.get('/services/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) }
-            const result=await servicesCollection.findOne(query)
+            const result = await servicesCollection.findOne(query)
             res.send(result);
         })
-        
+
         //post Api
         app.post('/services', async (req, res) => {
             const service = req.body;
-            console.log('hit hte post api',service);
+            console.log('hit hte post api', service);
 
             const result = await servicesCollection.insertOne(service);
             console.log(result);
