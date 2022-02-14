7077f5ddcf235f25caeb0491b129a118f7f860e8
sultanmamun5566 <sultanmamun5566@gmail.com>
2022-02-15 02:01:20 +0600

    added hello

--- index.js
+++ index.js
@@ -58,11 +58,13 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
 app.get('/', (req, res) => {
     res.send('Running Genius Server');
 });
+
+app.get('/hello', (req, res) => {
+    res.send('hello updated here')
+})
 
 app.listen(port, () => {
     console.log('On Port', port);
